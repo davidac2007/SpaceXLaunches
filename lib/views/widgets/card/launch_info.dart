001051cf91002b5e9b3ef10939182b6c2723a8ca
David Avendaño <d.avendano.c@hotmail.com>
2021-09-19 15:17:57 -0500

    Redesigned the card UI

--- lib/views/widgets/card/launch_info.dart
+++ lib/views/widgets/card/launch_info.dart
@@ -1,5 +1,9 @@
 import 'package:app_lanzamientos/models/launches.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 
 class LaunchInfo extends StatelessWidget {
   const LaunchInfo(this.launch, {Key? key}) : super(key: key);
@@ -13,34 +17,48 @@ class LaunchInfo extends StatelessWidget {
           Text(
             launch.name!,
             style: const TextStyle(
-                fontSize: 18.0,
+                fontSize: 20.0,
                 color: Colors.white,
-                fontWeight: FontWeight.bold),
+                fontWeight: FontWeight.w900),
           ),
           separator(),
-          Text(
-            "Fecha de lanzamiento: ${launch.dateLocal?.day.toString()}/${launch.dateLocal?.month.toString()}/${launch.dateLocal?.year.toString()}",
-            style: const TextStyle(fontSize: 16.0, color: Colors.white),
-          ),
+          styledText(
+              "Fecha de lanzamientos:",
+              " ${launch.dateLocal?.day.toString()}/${launch.dateLocal?.month.toString()}/${launch.dateLocal?.year.toString()}",
+              TextAlign.start),
           separator(),
-          Text(
-            launch.details == null
-                ? "No hay detalles"
-                : "Detalles: ${launch.details!}.",
-            textAlign: TextAlign.justify,
-            style: const TextStyle(fontSize: 16.0, color: Colors.white),
-          ),
+          styledText(
+              "Detalles:\n",
+              launch.details != null ? "${launch.details}." : "No hay detalles",
+              TextAlign.justify),
           separator(),
-          Text(
-            "Número de vuelo: ${launch.flightNumber.toString()}",
-            style: const TextStyle(fontSize: 16.0, color: Colors.white),
-          )
+          styledText("Número de vuelo: ", launch.flightNumber.toString(),
+              TextAlign.start)
         ],
       ),
     );
   }
 
   Widget separator() {
-    return const SizedBox(height: 5.0);
+    return const SizedBox(height: 7.0);
+  }
+
+  Widget styledText(String title, String body, TextAlign align) {
+    return RichText(
+      textAlign: align,
+      text: TextSpan(
+          text: title,
+          style: TextStyle(
+              color: Colors.lightBlue[900],
+              fontWeight: FontWeight.bold,
+              fontSize: 17.0),
+          children: <TextSpan>[
+            TextSpan(
+              text: body,
+              style: const TextStyle(
+                  color: Colors.white, fontWeight: FontWeight.normal),
+            )
+          ]),
+    );
   }
 }
